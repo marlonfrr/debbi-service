60a3c28187b1c51e5378a7cc1b72775b7388658d
jfelipeforero <felipe__2001@hotmail.com>
2022-10-11 10:03:07 -0500

    Heroku

--- src/routes/hook.router.ts
+++ src/routes/hook.router.ts
@@ -2,7 +2,7 @@ import express, { Request, Response } from 'express';
 
 const router = express.Router();
 
-router.get('/', (req: Request, res: Response) => {
+router.post('/', (req: Request, res: Response) => {
   console.log(req.body); // Call your action on the request here
   res.status(200).send({ msg: 'Hola' }); // Responding is important
 });
